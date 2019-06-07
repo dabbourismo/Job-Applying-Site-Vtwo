701f8fdb6a80f395e132ddc001e3f15938492b71
Mohamed Dabbour <gwars2moha@gmail.com>
2019-06-07 16:56:12 +0200

    Commit 1

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdentityComponents.Models;
+using JobApplicationSiteVtwo.Models;
+
+namespace JobApplicationSiteVtwo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(db.CategoryModels.ToList());
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryModel categoryModel = db.CategoryModels.Find(id);
+            if (categoryModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoryModel);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CategoryName,CategoryDescription")] CategoryModel categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CategoryModels.Add(categoryModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoryModel);
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryModel categoryModel = db.CategoryModels.Find(id);
+            if (categoryModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoryModel);
+        }
+
+        // POST: Category/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CategoryName,CategoryDescription")] CategoryModel categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoryModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoryModel);
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryModel categoryModel = db.CategoryModels.Find(id);
+            if (categoryModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoryModel);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CategoryModel categoryModel = db.CategoryModels.Find(id);
+            db.CategoryModels.Remove(categoryModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityComponents.Startup))]
+namespace IdentityComponents
+{
+    public partial class Startup
+    {
+        //herereeeeeeeeeeeeeeeeeeeeee
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
